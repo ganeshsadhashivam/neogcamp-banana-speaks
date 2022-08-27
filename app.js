5e8ce2d64a65a210a0aa6a2919222cec54a63d02
ganeshsadhashivam <ganeshsadhashivam@outlook.com>
2022-08-28 02:00:43 +0530

    Added
    added banana talk

---
+++ app.js
@@ -0,0 +1,48 @@
+var userInput = document.querySelector("#txt-input");
+
+
+ var btnTranslate = document.querySelector("#btn-translate");
+   
+
+ var outputDiv = document.querySelector("#output");
+
+     
+
+//var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+
+    return  serverURL + "?" + "text=" + text;
+}
+
+
+function errorHandler(error){
+     console.log("error occured",error);
+     alert("something wrong in server try again after some time");
+}
+
+
+function clickEventHandler(){
+
+     var inputText = userInput.value;
+
+     fetch(getTranslationURL(inputText))
+     .then(response => response.json())
+     .then(json =>{ 
+          var outputText =json.contents.translated;
+          outputDiv.innerText = outputText;
+     
+     }
+          )
+     .catch(errorHandler)
+};
+
+
+
+     btnTranslate.addEventListener("click",clickEventHandler);
+
+    
+
+    
